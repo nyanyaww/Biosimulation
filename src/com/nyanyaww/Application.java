a2b4cd8ea827f529643c6c06d899d949943d4c54
lcs <515478826@qq.com>
2019-07-14 22:55:20 +0800

    我总算是用起来fastjson了

---
+++ src/com/nyanyaww/Application.java
@@ -0,0 +1,31 @@
+package com.nyanyaww;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.List;
+
+/**
+ * @author nyanyaww
+ * @program Biosimulation
+ * @description 程序的入口
+ * @create 2019-07-14 22:09
+ **/
+public class Application {
+
+    public void testJson() {
+        JSONObject object = new JSONObject();
+        object.put("hello", 11);
+        String a = "casadqwe";
+        char[] c = new char[10];
+        c[0] = '1';
+        object.put("a", a);
+        object.put("c", c);
+        System.out.print(object);
+    }
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.testJson();
+    }
+
+}
